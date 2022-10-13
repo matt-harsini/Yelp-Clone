aeb8abc9cc5dbde6cac15abb0c270933a6527a36
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-10-13 16:01:18 -0700

    run

--- server/server.js
+++ server/server.js
@@ -147,7 +147,7 @@ app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
 
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3006;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
 });
