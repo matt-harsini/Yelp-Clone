77010d9d283c13431af2819f5b00a76ab85ef115
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-10-13 17:01:12 -0700

    test

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "api/v1"
+    ? "/api/v1"
     : "http://localhost:3001/api/v1";
 
 const client = axios.create({
   baseURL,
 });
-    
-export { baseURL, client};
+
+export { baseURL, client };
--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
-import {client} from "../apis/RestaurantFinder";
+import { client } from "../apis/RestaurantFinder";
 import { StarRating } from "../components/StarRating";
 import Reviews from "../components/Reviews";
 import { AddReview } from "../components/AddReview";
